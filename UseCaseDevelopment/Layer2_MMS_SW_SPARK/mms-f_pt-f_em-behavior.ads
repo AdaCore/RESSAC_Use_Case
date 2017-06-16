3b349cfd8ba6fb85e0f98b348dd3388b57e2e479
Anthony Leonardo Gracio <leonardo@adacore.com>
2017-06-16 10:55:47 +0200

    Identify constants used by MMS.F_PT.F_EM

---
+++ UseCaseDevelopment/Layer2_MMS_SW_SPARK/mms-f_pt-f_em-behavior.ads
@@ -0,0 +1,12 @@
+package MMS.F_PT.F_EM.Behavior is
+
+   ---------------
+   -- Constants --
+   ---------------
+   
+   --  From 6.8.4
+   
+   Primary_Initial_Capacity   : Energy_Level_Type;
+   Secondary_Initial_CapacitY : Energy_Level_Type;
+
+end MMS.F_PT.F_EM.Behavior;
